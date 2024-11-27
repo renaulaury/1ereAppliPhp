52934ad5450919b42d383c9b9a7a7f22a7710f8f
Laury <112404773+renaulaury@users.noreply.github.com>
2024-11-27 13:40:20 +0100

    maj

--- appli/traitement.php
+++ appli/traitement.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
 
-if (isset($_POST['submit'])) { //vérif de la clé submit dans le tableau post du formulaire
-}                               // si elle existe elle enverra vraie
+if (isset($_POST['submit'])) { //vérif de la clé submit dans le tableau post du formulaire si elle existe elle enverra vraie
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+    if($name && $price && $qtt) {
+        $product = [
+            "name" => $name,
+            "price" => $price,
+            "qtt" => $qtt,
+            "total" => $price * $qtt
+        ];
+    }
+}                               
 
 header("location:index.php"); //si non renvoie index.php
