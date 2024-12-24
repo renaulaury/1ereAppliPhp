5098f1470545d14ac2503cc2b3db805ca2277647
Lily <112404773+Belywen@users.noreply.github.com>
2024-12-24 21:15:30 +0100

    maj

--- appli/recap.php
+++ appli/recap.php
@@ -4,6 +4,7 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,8 +12,9 @@ session_start();
     <link rel="stylesheet" href="../css/styles.css">
     <title>Récap des produits</title>
 </head>
+
 <body>
-<header>
+    <header>
         <nav id="navbar">
             <ul>
                 <li><a href="./index.php">Index</a></li>
@@ -20,57 +22,58 @@ session_start();
             </ul>
         </nav>
     </header>
-    
-<main id="recap">
-    <h1>Récapitulatif de commande</h1>
 
-    <div class="commande">
+    <main id="recap">
+        <h1>Récapitulatif de commande</h1>
 
-        <?php 
-            if(!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+        <div class="commande">
+
+            <?php
+            if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
                 echo "<p>Aucun produit en session...</p>";
-            }  else {
+            } else {
                 echo "<table>",
-                    "<thead class='entete'>",
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                        "</tr>",
-                    "</thead>",
-                    "<tbody>";
+                "<thead class='entete'>",
+                "<tr>",
+                "<th>#</th>",
+                "<th>Nom</th>",
+                "<th>Prix</th>",
+                "<th>Quantité</th>",
+                "<th>Total</th>",
+                "</tr>",
+                "</thead>",
+                "<tbody>";
 
                 $totalGeneral = 0;
 
-                foreach($_SESSION['products'] as $index => $product) {
+                foreach ($_SESSION['products'] as $index => $product) {
                     echo "<tr>",
-                            "<td>".$index."</td>",
-                            "<td>".$product['name']."</td>",
-                            "<td>".number_format($product['price'], 2, ",", "&nbsp;"). "&nbsp;€</td>",
-                            // "<td>".$product['price']."</td>",
-                            "<td><a class='lien' href='traitement.php?action=up-qtt&id=$index'>+</a>".$product['qtt']."<a class='lien' href='traitement.php?action=down-qtt&id=$index'>-</a></td>",
-                            "<td>".number_format($product['total'], 2, ",", "&nbsp;"). "&nbsp;€</td>",
-                            // "<td>".$product['total']."</td>",
-                            "<td class='col'><a class='lien' href='traitement.php?action=delete&id=$index'><i class='fa-solid fa-trash'></i></td>",
-                        "</tr>";
-                        $totalGeneral += $product['total'];
+                    "<td>" . $index . "</td>",
+                    "<td>" . $product['name'] . "</td>",
+                    "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
+                    // "<td>".$product['price']."</td>",
+                    "<td><a class='lien' href='traitement.php?action=up-qtt&id=$index'>+</a>" . $product['qtt'] . "<a class='lien' href='traitement.php?action=down-qtt&id=$index'>-</a></td>",
+                    "<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
+                    // "<td>".$product['total']."</td>",
+                    "<td class='col'><a class='lien' href='traitement.php?action=delete&id=$index'><i class='fa-solid fa-trash'></i></td>",
+                    "</tr>";
+                    $totalGeneral += $product['total'];
                 }
 
                 echo "<tr class='tot'>",
-                        "<td colspan=4 class='total'>Total Général : </td>",
-                        "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp"), "&nbsp;€</strong></td>",
-                        "<td class='col'><a class='lien' href='traitement.php?action=clear'><i class='fa-solid fa-dumpster-fire'></i></td>",
-                    "</tr>",
-                    "</tbody>",
-                "</table>";      
+                "<td colspan=4 class='total'>Total Général : </td>",
+                "<td><strong>" . number_format($totalGeneral, 2, ",", "&nbsp"), "&nbsp;€</strong></td>",
+                "<td class='col'><a class='lien' href='traitement.php?action=clear'><i class='fa-solid fa-dumpster-fire'></i></td>",
+                "</tr>",
+                "</tbody>",
+                "</table>";
             }
 
-        ?>
-    </div>
-</main>
+            ?>
+        </div>
+    </main>
 
 
 </body>
+
 </html>
